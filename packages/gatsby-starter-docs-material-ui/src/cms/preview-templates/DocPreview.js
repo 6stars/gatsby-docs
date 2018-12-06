fe28ba6cf609d053d1e94fa8fc883e4803a6b231
Adam Moon <adamjmoon@gmail.com>
2018-12-06 00:02:43 -0600

    logo update, adjust cms config

--- packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreview.js
+++ packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreview.js
@@ -1,6 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { DocTemplate } from '../../templates/doc'
+import React from 'react';
+import PropTypes from 'prop-types';
+import DocTemplate from '../../templates/doc';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    marginBottom: 100,
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+  },
+  markdownElement: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+    padding: `0 ${theme.spacing.unit}px`,
+  },
+});
 
 const DocPreview = ({ entry, widgetFor }) => (
   <DocTemplate
@@ -18,4 +35,6 @@ DocPreview.propTypes = {
   widgetFor: PropTypes.func,
 }
 
-export default DocPreview
+
+
+export default withStyles(DocPreview, styles);
