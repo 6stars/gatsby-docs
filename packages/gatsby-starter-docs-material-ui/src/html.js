b80125aff2ad8fa4787c043b8acdb6565f4b0d17
Adam Moon <amoon@defisolutions.com>
2019-01-02 22:14:35 -0600

    fix: ios paths

--- packages/gatsby-starter-docs-material-ui/src/html.js
+++ packages/gatsby-starter-docs-material-ui/src/html.js
@@ -35,19 +35,19 @@ export default class HTML extends React.Component {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
           />
-          <link rel="apple-ios-appicon" href="ios/ios-appicon-iphone.png" />
-          <link rel="apple-ios-appicon" sizes="152x152" href="ios/ios-appicon-ipad.png" />
-          <link rel="apple-ios-appicon" sizes="180x180" href="ios/ios-appicon-iphone-retina.png" />
-          <link rel="apple-ios-appicon" sizes="167x167" href="ios/ios-appicon-ipad-retina.png" />          
-          <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" href="ios/ios-launchimage-1242x2688.png" />          
-          <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" href="ios/ios-launchimage-828x1792.png" />          
-          <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="ios/ios-launchimage-1125x2436.png" />
-          <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)" href="ios/ios-launchimage-1242x2208.png" />          
-          <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" href="ios/ios-launchimage-750x1334.png" />          
-          <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" href="ios/ios-launchimage-2048x2732.png" />          
-          <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" href="ios/ios-launchimage-1668x2388.png" />          
-          <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" href="ios/ios-launchimage-1668x2224.png" />          
-          <link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="ios/ios-launchimage-1536x2048.png" />       
+          <link rel="apple-ios-appicon" href="/ios/ios-appicon-iphone.png" />
+          <link rel="apple-ios-appicon" sizes="152x152" href="/ios/ios-appicon-ipad.png" />
+          <link rel="apple-ios-appicon" sizes="180x180" href="/ios/ios-appicon-iphone-retina.png" />
+          <link rel="apple-ios-appicon" sizes="167x167" href="/ios/ios-appicon-ipad-retina.png" />          
+          <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" href="/ios/ios-launchimage-1242x2688.png" />          
+          <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" href="/ios/ios-launchimage-828x1792.png" />          
+          <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="/ios/ios-launchimage-1125x2436.png" />
+          <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)" href="/ios/ios-launchimage-1242x2208.png" />          
+          <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" href="/ios/ios-launchimage-750x1334.png" />          
+          <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" href="/ios/ios-launchimage-2048x2732.png" />          
+          <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" href="/ios/ios-launchimage-1668x2388.png" />          
+          <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" href="/ios/ios-launchimage-1668x2224.png" />          
+          <link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="/ios/ios-launchimage-1536x2048.png" />       
           {/*
             Preconnect allows the browser to setup early connections before an HTTP request
             is actually sent to the server.
