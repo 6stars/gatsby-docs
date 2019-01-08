03911683608456f8972378813f7f6fbb2864e0c4
Adam Moon <amoon@defisolutions.com>
2019-01-08 00:53:30 -0600

    fix: css injector

--- packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreviewTemplate.js
+++ packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreviewTemplate.js
@@ -14,12 +14,16 @@ let store = initRedux( { theme: light } );
 // withRoot returns Functional Component that raps children passed as props
 let WithRoot = withRoot(props => props.children);
 
+const css = muiPageContext.sheetsRegistry.toString();
 
-const DocPreviewTemplate = () => {
-// return WithRoot component wrapping Root Element with muiPageContext and redux store as props
-return (
-    <WithRoot key={Math.random()} muiPageContext={muiPageContext} store={store}>{DocPreview}</WithRoot>
-);
+const DocPreviewTemplate = (props) => {
+    // return WithRoot component wrapping Root Element with muiPageContext and redux store as props
+    return (
+        <CSSInjector css={css}>
+            <DocPreview {...props} />
+        </CSSInjector>
+       
+    );
 }
 
 export default DocPreviewTemplate;
