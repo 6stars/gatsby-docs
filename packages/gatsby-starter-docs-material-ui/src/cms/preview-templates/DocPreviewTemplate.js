82b12019025f3a8abdbb6b1b5fd8955bc8eacfe3
Adam Moon <amoon@defisolutions.com>
2019-01-08 07:28:43 -0600

    fix: PreviewTemplate

--- packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreviewTemplate.js
+++ packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreviewTemplate.js
@@ -14,15 +14,10 @@ let store = initRedux( { theme: light } );
 // withRoot returns Functional Component that raps children passed as props
 let WithRoot = withRoot(props => props.children);
 
-const css = muiPageContext.sheetsRegistry.toString();
-
-const DocPreviewTemplate = (props) => {
+const DocPreviewTemplate = () => {
     // return WithRoot component wrapping Root Element with muiPageContext and redux store as props
     return (
-        <CSSInjector css={css}>
-            <DocPreview {...props} />
-        </CSSInjector>
-       
+        <WithRoot key={Math.random()} muiPageContext={muiPageContext} store={store}>{DocPreview}</WithRoot>
     );
 }
 
