a696a060b6a8a97a9eb195690669ad655b3f9b32
Adam Moon <amoon@defisolutions.com>
2019-01-08 00:18:42 -0600

    fix: extra /

--- packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreviewTemplate.js
+++ packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreviewTemplate.js
@@ -2,25 +2,25 @@ import React from 'react';
 import withRoot from '../../src/withRoot';
 import createPageContext from '../../src/getPageContext';
 import initRedux from '../../src/redux/initRedux';
-import light from '../..//src/styles/light';
+import light from '../../src/styles/light';
 import DocPreview from './DocPreview';
 
-  // create material Page Context
-  let muiPageContext = createPageContext();
-  
-  // initial setup for Redux Store
-  let store = initRedux( { theme: light } );
- 
-  // withRoot returns Functional Component that raps children passed as props
-  let WithRoot = withRoot(props => props.children);
-  
+// create material Page Context
+let muiPageContext = createPageContext();
 
-  const DocPreviewTemplate = () => {
-    // return WithRoot component wrapping Root Element with muiPageContext and redux store as props
-    return (
-        <WithRoot key={Math.random()} muiPageContext={muiPageContext} store={store}>{DocPreview}</WithRoot>
-    );
-  }
+// initial setup for Redux Store
+let store = initRedux( { theme: light } );
 
-  export default DocPreviewTemplate;
+// withRoot returns Functional Component that raps children passed as props
+let WithRoot = withRoot(props => props.children);
+
+
+const DocPreviewTemplate = () => {
+// return WithRoot component wrapping Root Element with muiPageContext and redux store as props
+return (
+    <WithRoot key={Math.random()} muiPageContext={muiPageContext} store={store}>{DocPreview}</WithRoot>
+);
+}
+
+export default DocPreviewTemplate;
   
