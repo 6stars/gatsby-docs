015209e67739906b6934f527810e47fe9b2e18da
Adam Moon <amoon@defisolutions.com>
2019-01-08 09:56:22 -0600

    fix: doc

--- packages/gatsby-starter-docs-material-ui/gatsby-config.js
+++ packages/gatsby-starter-docs-material-ui/gatsby-config.js
@@ -165,7 +165,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        modulePath: `${__dirname}/cms/cms.js`,
       },
     },
     {
