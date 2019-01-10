75dd9e701e7973819f83a03f1d2ddfc0c3c10b1c
Adam Moon <amoon@defisolutions.com>
2019-01-09 21:43:40 -0600

    fix: try adding logs

--- packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreviewTemplate.js
+++ packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/DocPreviewTemplate.js
@@ -4,27 +4,31 @@ import withRoot from '../../withRoot';
 import createPageContext from '../../getPageContext';
 import initRedux from '../../redux/initRedux';
 import light from '../../styles/light';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { setPrismTheme } from '../styles/prism';
 import DocPreview from './DocPreview';
 // create material Page Context
 let muiPageContext = createPageContext();
 
-// // initial setup for Redux Store
-let store = initRedux( { theme: light } );
-
-// // withRoot returns Functional Component that raps children passed as props
-let WithRoot = withRoot(props => props.children);
-
 const DocPreviewTemplate = ({ entry }) => {
       return (
-        <WithRoot key={Math.random()} muiPageContext={muiPageContext} store={store}>
-            <DocPreview
-                content={entry.getIn(["data", "body"])}
+        <JssProvider
+            jss={muiPageContext.jss}
+            registry={muiPageContext.sheetsRegistry}
+            generateClassName={muiPageContext.generateClassName}
+            >      
+            <MuiThemeProvider theme={muiPageContext.theme} sheetsManager={muiPageContext.sheetsManager}>
+            <CssBaseline />          
+            <DocPreview content={entry.getIn(["data", "body"])}
                 tags={entry.getIn(['data', 'tags'])}
                 title={entry.getIn(['data', 'title'])}
                 rawDate={entry.getIn(['data', 'date'])}
                 category={entry.getIn(['data', 'category'])}
             />
-        </WithRoot>
+            </MuiThemeProvider>
+        </JssProvider>
     );
   }
 
