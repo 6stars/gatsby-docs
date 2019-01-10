6e52ef61f05e6989baa21096a4dd5b9931659917
Adam Moon <amoon@defisolutions.com>
2019-01-10 14:40:52 -0600

    fix: try CSSInjector again

--- packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/CssInjector.js
+++ packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/CssInjector.js
@@ -2,14 +2,17 @@ import React from "react"
 
 class CSSInjector extends React.Component {
   render() {
-    return (
+    const iframe = document.querySelector(".nc-previewPane-frame")
+    const iframeHeadElem = iframe.contentDocument.head;
+
+    return React.forwardRef((props, ref) => (
       <div
         ref={ref => {
-            ref.ownerDocument.head.innerHTML += this.props.css
+            ref.ownerDocument.head.innerHTML += props.css
         }}>
-        {React.Children.only(this.props.children)}
+        {React.Children.only(props.children)}
       </div>
-    )
+    ));
   }
 }
 
