1c866de1a10a0c386a9eadc6041b42db4a933824
Adam Moon <amoon@defisolutions.com>
2019-01-10 16:39:24 -0600

    adjust css injection

--- packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/CssInjector.js
+++ packages/gatsby-starter-docs-material-ui/src/cms/preview-templates/CssInjector.js
@@ -1,19 +1,12 @@
 import React from "react"
 
-class CSSInjector extends React.Component {
-  render() {
-
-    return React.forwardRef((props, ref) => {
+const CSSInjector = React.forwardRef((props, ref) => {
         ref.ownerDocument.head.innerHTML += props.css
         console.log(ref.ownerDocument.head.innerHTML);
         return (
             <React.Fragment>
-                 
                 {React.Children.only(props.children)}
             </React.Fragment>
         )
     });
-  }
-}
-
 export default CSSInjector;
