dd609f67ce67711a1a66856188e6f5a56c755eb4
Adam Moon <amoon@defisolutions.com>
2019-01-14 12:26:15 -0600

    adjust logo name

--- packages/gatsby-starter-docs-material-ui/src/components/AppDrawer.js
+++ packages/gatsby-starter-docs-material-ui/src/components/AppDrawer.js
@@ -1,54 +1,52 @@
-import React from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import Drawer from '@material-ui/core/Drawer';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Divider from '@material-ui/core/Divider';
-import Hidden from '@material-ui/core/Hidden';
-import Link from 'gatsby-link';
-import AppDrawerNavItem from './AppDrawerNavItem';
-import Logo from '../../static/img/logo_transparent.png';
+import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import Drawer from '@material-ui/core/Drawer'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import Divider from '@material-ui/core/Divider'
+import Hidden from '@material-ui/core/Hidden'
+import Link from 'gatsby-link'
+import AppDrawerNavItem from './AppDrawerNavItem'
+import Logo from '../../static/img/logo.png'
 
 const styles = theme => {
-  
-  return (
-  {
-  paper: {
-    width: 240,
-    backgroundColor: theme.palette.background.default,
-  },
-  title: {
-    color: theme.palette.text.secondary,
-    marginBottom: theme.spacing.unit / 2,
-    '&:hover': {
-      color: theme.palette.primary.main,
+  return {
+    paper: {
+      width: 240,
+      backgroundColor: theme.palette.background.default,
     },
-  },
-  img: {
-    width: 170,
-    marginTop: 7,
-  },
-  // https://github.com/philipwalton/flexbugs#3-min-height-on-a-flex-container-wont-apply-to-its-flex-items
-  toolbarIe11: {
-    display: 'flex',
-  },
-  toolbar: {
-    ...theme.mixins.toolbar,
-    paddingLeft: theme.spacing.unit * 3,
-    display: 'flex',
-    flexGrow: 1,
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    backgroundColor: theme.palette.primary.main,
-  },
-  anchor: {
-    color: theme.palette.text.secondary,
-  },
-})
-};
+    title: {
+      color: theme.palette.text.secondary,
+      marginBottom: theme.spacing.unit / 2,
+      '&:hover': {
+        color: theme.palette.primary.main,
+      },
+    },
+    img: {
+      width: 170,
+      marginTop: 7,
+    },
+    // https://github.com/philipwalton/flexbugs#3-min-height-on-a-flex-container-wont-apply-to-its-flex-items
+    toolbarIe11: {
+      display: 'flex',
+    },
+    toolbar: {
+      ...theme.mixins.toolbar,
+      paddingLeft: theme.spacing.unit * 3,
+      display: 'flex',
+      flexGrow: 1,
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      justifyContent: 'center',
+      backgroundColor: theme.palette.primary.main,
+    },
+    anchor: {
+      color: theme.palette.text.secondary,
+    },
+  }
+}
 
 // eslint-disable-next-line react/prop-types
 function renderNavItems({ pages, ...params }) {
@@ -57,29 +55,39 @@ function renderNavItems({ pages, ...params }) {
       {pages.reduce(
         // eslint-disable-next-line no-use-before-define
         (items, page) => reduceChildRoutes({ items, page, ...params }),
-        [],
+        []
       )}
     </List>
-  );
+  )
 }
 
 function reduceChildRoutes({ props, activePage, items, page, depth }) {
   if (page.displayNav === false) {
-    return items;
+    return items
   }
-  
+
   if (page.isEmptyParent) {
-    const title = page.title;
-    const openImmediately = activePage.indexOf(`${page.path}/`) === 0;
-    
+    const title = page.title
+    const openImmediately = activePage.indexOf(`${page.path}/`) === 0
+
     items.push(
-      <AppDrawerNavItem depth={depth} key={title} openImmediately={openImmediately} title={title}>
-        {renderNavItems({ props, pages: page.children, activePage, depth: depth + 1 })}
-      </AppDrawerNavItem>,
-    );
+      <AppDrawerNavItem
+        depth={depth}
+        key={title}
+        openImmediately={openImmediately}
+        title={title}
+      >
+        {renderNavItems({
+          props,
+          pages: page.children,
+          activePage,
+          depth: depth + 1,
+        })}
+      </AppDrawerNavItem>
+    )
   } else {
-    const title = page.title;
-    page = page.children && page.children.length === 1 ? page.children[0] : page;
+    const title = page.title
+    page = page.children && page.children.length === 1 ? page.children[0] : page
 
     items.push(
       <AppDrawerNavItem
@@ -88,37 +96,50 @@ function reduceChildRoutes({ props, activePage, items, page, depth }) {
         title={title}
         href={page.path}
         onClick={props.onClose}
-      />,
-    );
+      />
+    )
   }
 
-  return items;
+  return items
 }
 
 // iOS is hosted on high-end devices. We can enable the backdrop transition without
 // dropping frames. The performance will be good enough.
 // So: <SwipeableDrawer disableBackdropTransition={false} />
-const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent)
 
 function AppDrawer(props) {
-  const { classes, className, disablePermanent, mobileOpen, onClose, onOpen, currentPage } = props;
-  
+  const {
+    classes,
+    className,
+    disablePermanent,
+    mobileOpen,
+    onClose,
+    onOpen,
+    currentPage,
+  } = props
+
   const renderLogo = () => {
-    return <img src={Logo} alt="logo" width="170" className={classes.img} />;
-  };
+    return <img src={Logo} alt="logo" width="170" className={classes.img} />
+  }
   const drawer = (
     <div className={classes.nav}>
       <div className={classes.toolbarIe11}>
         <div className={classes.toolbar}>
           <Link className={classes.title} to="/">
-             {renderLogo()}
+            {renderLogo()}
           </Link>
         </div>
       </div>
       <Divider />
-      {renderNavItems({ props, pages: currentPage && currentPage.tree ? currentPage.tree : [], activePage: currentPage ? currentPage.path : '', depth: 0 })}
+      {renderNavItems({
+        props,
+        pages: currentPage && currentPage.tree ? currentPage.tree : [],
+        activePage: currentPage ? currentPage.path : '',
+        depth: 0,
+      })}
     </div>
-  );
+  )
 
   return (
     <nav className={className}>
@@ -153,7 +174,7 @@ function AppDrawer(props) {
         </Hidden>
       )}
     </nav>
-  );
+  )
 }
 
 AppDrawer.propTypes = {
@@ -164,6 +185,6 @@ AppDrawer.propTypes = {
   onClose: PropTypes.func.isRequired,
   onOpen: PropTypes.func.isRequired,
   currentPage: PropTypes.object.isRequired,
-};
+}
 
-export default withStyles(styles)(AppDrawer);
+export default withStyles(styles)(AppDrawer)
