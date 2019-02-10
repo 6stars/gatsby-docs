e97bc7b200ca2185f3e163f8e41d2caeb3b08a2a
Adam Moon <amoon@defisolutions.com>
2019-02-09 22:52:27 -0600

    commit root yarn.lock file

--- packages/docs-a/data/SiteConfig.js
+++ packages/docs-a/data/SiteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteTitle: '6star DOCs', // Site title.
-  siteTitleShort: '6star Specs', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.  
+  siteTitleShort: '6star Specs', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: '6star DOCS', // Alternative site title for SEO.
-  siteDescription: "for 6star Design Specs",
+  siteDescription: 'for 6star Design Specs',
   siteLogo: '/upload/logo.png', // Logo used for SEO and manifest.
   siteUrl: 'https://6star.pw', // Domain of your website without pathPrefix.
   siteRss: '/rss.xml', // Path to the RSS file.
@@ -11,6 +11,6 @@ module.exports = {
   docDefaultCategoryID: 'guide', // Default category for docs.
   dateFromFormat: 'MM-DD-YYYY', // Date format used in the frontmatter.
   dateFormat: 'MM/DD/YYYY', // Date format for display.
-  userName: '****** User', // Username to display in the author segment.
-  copyright: 'Copyright © 2019. ******', // Copyright string for the footer of the website and RSS feed.
-};
+  userName: '6star User', // Username to display in the author segment.
+  copyright: 'Copyright © 2019. 6star', // Copyright string for the footer of the website and RSS feed.
+}
