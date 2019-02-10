09fb86153c4a32e27bd815b6f5945736131e3d51
Adam Moon <43415566+6stars@users.noreply.github.com>
2019-02-09 18:23:50 -0600

    Update SiteConfig.js

--- packages/docs-a/data/SiteConfig.js
+++ packages/docs-a/data/SiteConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  siteTitle: '6star Design Specs', // Site title.
+  siteTitle: '6star DOCs', // Site title.
   siteTitleShort: '6star Specs', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.  
-  siteTitleAlt: '6star SPEC DOCS', // Alternative site title for SEO.
+  siteTitleAlt: '6star DOCS', // Alternative site title for SEO.
   siteDescription: "for 6star Design Specs",
   siteLogo: '/upload/logo.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://docs-b.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://6star.pw', // Domain of your website without pathPrefix.
   siteRss: '/rss.xml', // Path to the RSS file.
   pathPrefix: '/docs', // Prefixes all links.
   siteGATrackingID: '', // Tracking code ID for google analytics.
@@ -12,5 +12,5 @@ module.exports = {
   dateFromFormat: 'MM-DD-YYYY', // Date format used in the frontmatter.
   dateFormat: 'MM/DD/YYYY', // Date format for display.
   userName: 'Defi User', // Username to display in the author segment.
-  copyright: 'Copyright © 2019. defi SOLUTIONS', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © 2019. ******', // Copyright string for the footer of the website and RSS feed.
 };
