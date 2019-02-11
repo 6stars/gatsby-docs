7fa9ea334d9af8041e0505096f7d92228c5a56ac
Adam Moon <amoon@defisolutions.com>
2019-02-10 19:31:20 -0600

    add resolution 'ajv': '6.8.1' because 6.9.0 bug broke webpack https://github.com/webpack/webpack/issues/8768?source=techstories.org

--- packages/docs-b/data/SiteConfig.js
+++ packages/docs-b/data/SiteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteTitle: '6star DOCS', // Site title.
-  siteTitleShort: '6star DOCS', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.  
+  siteTitleShort: '6star DOCS', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: '6star DOCS', // Alternative site title for SEO.
-  siteDescription: "for 6star",
+  siteDescription: 'for 6star',
   siteLogo: '/upload/logo.png', // Logo used for SEO and manifest.
   siteUrl: 'https://6star.pw', // Domain of your website without pathPrefix.
   siteRss: '/rss.xml', // Path to the RSS file.
@@ -11,6 +11,6 @@ module.exports = {
   docDefaultCategoryID: 'guide', // Default category for docs.
   dateFromFormat: 'MM-DD-YYYY', // Date format used in the frontmatter.
   dateFormat: 'MM/DD/YYYY', // Date format for display.
-  userName: 'Defi User', // Username to display in the author segment.
-  copyright: 'Copyright © 2019. defi SOLUTIONS', // Copyright string for the footer of the website and RSS feed.
-};
+  userName: '******* User', // Username to display in the author segment.
+  copyright: 'Copyright © 2019. ******', // Copyright string for the footer of the website and RSS feed.
+}
--- tmp/gatsby-starter-docs-material-ui/gatsby-config.js
+++ tmp/gatsby-starter-docs-material-ui/gatsby-config.js
@@ -1,8 +1,8 @@
-const config = require("./data/SiteConfig");
-const iconsConfig = require("./static/icons.json");
-const urljoin = require("url-join");
+const config = require('./data/SiteConfig')
+const iconsConfig = require('./static/icons.json')
+const urljoin = require('url-join')
 
-const regexExcludeRobots = /^(?!\/(dev-404-page|404|offline-plugin-app-shell-fallback|tags|categories)).*$/;
+const regexExcludeRobots = /^(?!\/(dev-404-page|404|offline-plugin-app-shell-fallback|tags|categories)).*$/
 
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
@@ -33,79 +33,74 @@ const allDocsPageQuery = `{
       }	
     }	
   }
-}`;
+}`
 
 const queries = [
   {
     query: allDocsPageQuery,
-    transformer: ({ data }) => data.allMarkdownRemark.edges.map(({ node }) => node), // optional    
+    transformer: ({ data }) =>
+      data.allMarkdownRemark.edges.map(({ node }) => node), // optional
     indexName: '6stars-docs', // overrides main index name, optional
   },
-];
-
+]
 
 module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
     siteUrl: urljoin(config.siteUrl, config.pathPrefix),
-    title: '****** DOCS'
+    title: '****** DOCS',
   },
   plugins: [
     'gatsby-plugin-typescript',
     {
-      resolve: "@m00n/gatsby-plugin-nav-tree",
+      resolve: '@m00n/gatsby-plugin-nav-tree',
       options: {
-        ignorePaths: ["/categories", "/tags",  "/404", "/offline-plugin-app-shell-fallback",
-        "/dev-404-page"]
-      }
+        ignorePaths: [
+          '/categories',
+          '/tags',
+          '/404',
+          '/offline-plugin-app-shell-fallback',
+          '/dev-404-page',
+        ],
+      },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-lodash",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-lodash',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "docs",
-        path: `${__dirname}/content`
-      }
+        name: 'docs',
+        path: `${__dirname}/content`,
+      },
     },
-    // {
-    //   resolve: `gatsby-plugin-algolia`,
-    //   options: {
-    //     appId: process.env.ALGOLIA_APP_ID,
-    //     apiKey: process.env.ALGOLIA_APIKEY,
-    //     indexName: process.env.ALGOLIA_INDEX_NAME,
-    //     queries,
-    //     chunkSize: 10000, // default: 1000
-    //   },
-    // },
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
             resolve: `gatsby-remark-sequence`,
             options: {
-              theme: 'simple'
-            }
-          },          
+              theme: 'simple',
+            },
+          },
           {
-            resolve: "gatsby-remark-embed-youtube",
+            resolve: 'gatsby-remark-embed-youtube',
             options: {
               width: 1000,
-              height: 400
-            }
+              height: 400,
+            },
           },
           {
-            resolve: "gatsby-remark-relative-images"
+            resolve: 'gatsby-remark-relative-images',
           },
           {
-            resolve: "gatsby-remark-images",
+            resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 672
-            }
-          },         
+              maxWidth: 672,
+            },
+          },
           {
-            resolve: "gatsby-remark-responsive-iframe"
+            resolve: 'gatsby-remark-responsive-iframe',
           },
           {
             resolve: `gatsby-remark-prismjs`,
@@ -117,7 +112,7 @@ module.exports = {
               // you may use this to prevent Prism from re-processing syntax.
               // This is an uncommon use-case though;
               // If you're unsure, it's best to use the default value.
-              classPrefix: "language-",
+              classPrefix: 'language-',
               // This is used to allow setting a language for inline code
               // (i.e. single backticks) by creating a separator.
               // This separator is a string and will do no white-space
@@ -143,45 +138,33 @@ module.exports = {
               noInlineHighlight: false,
             },
           },
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-autolink-headers"
-        ]
-      }
-    },   
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-autolink-headers',
+        ],
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: config.siteTitle,
         short_name: config.siteTitleShort,
         description: config.siteDescription,
         start_url: config.pathPrefix,
-        background_color: "#334058",
-        theme_color: "#6ec5ff",
-        display: "standalone",
-        icons: iconsConfig.icons
-      }
+        background_color: '#334058',
+        theme_color: '#6ec5ff',
+        display: 'standalone',
+        icons: iconsConfig.icons,
+      },
     },
     'gatsby-plugin-offline',
     {
-      resolve: "gatsby-plugin-netlify-cms",
+      resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-      },
-    },
     // make sure to put last in the array
-  ]
-};
+  ],
+}
