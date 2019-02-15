26b005de6c5e426577bfe8413da3cf18ed00017a
Adam Moon <amoon@defisolutions.com>
2019-02-15 15:16:18 -0600

    feat: http2 with netlify plugin

--- packages/docs-a/gatsby-config.js
+++ packages/docs-a/gatsby-config.js
@@ -173,6 +173,18 @@ module.exports = {
         icons: iconsConfig.icons,
       },
     },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: false, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
     'gatsby-plugin-offline',
     {
       resolve: 'gatsby-plugin-netlify-cms',
